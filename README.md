5be2959ce7bcd14ead336ea957d62f1a17891a48
aviboddu <aviboddu@gmail.com>
2026-01-04 12:59:47 -0800

    Initialize rust project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # insect-island
-A traditional roguelike set on an island of insects! Made in rust
+A traditional roguelike set on an island of insects! Made in Rust
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+//! Insect Island - A traditional roguelike set on an island of insects.
+/// Entry point of the application.
+fn main() {
+    println!("Hello, world!");
+}
