cc3e85d9af96bd147a9fb8891cbcf3b9c628239b
aviboddu <aviboddu@gmail.com>
2026-01-04 17:19:35 -0800

    Implemented font loading

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,23 @@ fn main() {
         .size(640, 480)
         .title(_config.get_string("app_name").unwrap().as_str())
         .build();
+    let resources = load_resources(&mut rl, &thread);
+    let font = resources
+        .fonts
+        .get("default.ttf")
+        .unwrap_or(&resources.fallback_font);
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::WHITE);
-        d.draw_text("Config Loaded", 12, 12, 20, Color::BLACK);
+        d.draw_text_ex(
+            font,
+            "Config Loaded",
+            Vector2::new(12.0, 12.0),
+            20.0,
+            2.0,
+            Color::BLACK,
+        );
     }
 }
--- src/utils/loading/mod.rs
+++ src/utils/loading/mod.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
+
 use config::{Config, File};
 use glob::glob;
+use raylib::prelude::*;
 
 pub fn load_config() -> Config {
     Config::builder()
@@ -13,6 +16,33 @@ pub fn load_config() -> Config {
         .expect("Failed to load configuration")
 }
 
+pub struct Resources {
+    pub fonts: HashMap<String, Font>,
+    pub fallback_font: Font,
+}
+
+pub fn load_resources(rl: &mut RaylibHandle, thread: &RaylibThread) -> Resources {
+    let files = glob("resources/**/*.ttf").unwrap();
+    let mut font_map: HashMap<String, Font> = HashMap::new();
+    for file in files {
+        let path = file.unwrap();
+        let file_path: String = path.clone().into_os_string().into_string().unwrap();
+        let font = rl.load_font(thread, file_path.as_str()).unwrap();
+        let file_name = path
+            .file_name()
+            .unwrap()
+            .to_os_string()
+            .into_string()
+            .unwrap();
+        font_map.insert(file_name, font);
+    }
+    let fallback_font = unsafe { Font::from_raw(rl.get_font_default().to_raw()) };
+    return Resources {
+        fonts: font_map,
+        fallback_font,
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,5 +52,13 @@ mod tests {
         let config = load_config();
         // Add assertions based on expected configuration values
         assert!(config.get_string("app_name").is_ok());
+    }
+
+    #[test]
+    fn test_load_resources() {
+        let (mut rl, thread) = raylib::init().size(640, 480).title("Test").build();
+        let resources = load_resources(&mut rl, &thread);
+        assert!(resources.fonts.is_empty());
+        assert!(resources.fallback_font.is_font_valid());
     }
 }
