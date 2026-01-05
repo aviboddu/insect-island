6cfaebb0033e8bb684b60bfeaf9d4b8c25fd7b20
aviboddu <aviboddu@gmail.com>
2026-01-04 22:23:51 -0800

    basic example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
 //! Insect Island - A traditional roguelike set on an island of insects.
 
+use std::time::Duration;
+
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, render::TextureAccess};
+
 /// Entry point of the application.
 fn main() {
     println!("Hello World");
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let _window = video_subsystem
+        .window("Insect Island", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = _window.into_canvas().build().unwrap();
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
